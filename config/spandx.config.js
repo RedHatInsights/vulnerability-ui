f23a642a7f9d13f4e6cb6d50ebb101902139c329
Katerina Patticha <apattich@redhat.com>
2020-01-21 13:55:08 +0100

    chore(eslint): fix eslint errors

--- config/spandx.config.js
+++ config/spandx.config.js
@@ -1,4 +1,4 @@
-/*global module, process*/
+/*global module*/
 
 // Hack so that Mac OSX docker can sub in host.docker.internal instead of localhost
 // see https://docs.docker.com/docker-for-mac/networking/#i-want-to-connect-from-a-container-to-a-service-on-the-host
