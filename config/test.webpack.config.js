f5d6c798e117661f34a5ae1d166163765f346c76
Katerina Patticha <apattich@redhat.com>
2020-11-12 09:23:53 +0100

    chore(config): Start using frontend-components-config

--- config/test.webpack.config.js
+++
@@ -1,9 +0,0 @@
-/* global require, module */
-
-const _ = require('lodash');
-const config = _.cloneDeep(require('./base.webpack.config'));
-
-module.exports = _.merge({},
-    config,
-    require('./test.webpack.plugins.js')
-);
